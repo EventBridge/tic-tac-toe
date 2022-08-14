2eda60d3a97161955b04e2e18b9ed6ac55a9b736
EventBridge <vincentwhitetran@gmail.com>
2022-08-14 22:15:25 +1000

    eslint integration

--- main.js
+++ main.js
@@ -1,40 +1,48 @@
 const gameBoard = (() => {
-    const board = [
-        ["O", "X", "O"],
-        ["O", "O", "X"],
-        ["X", "O", "O"],
-    ];
+  const board = [
+    ['O', 'X', 'O'],
+    ['O', 'O', 'X'],
+    ['X', 'O', 'O'],
+  ];
 
-    return {
-        board,
-    };
+  return {
+    board,
+  };
 })();
 
 const displayController = (() => {
-    const boardButtons = document.querySelectorAll("button");
+  const boardButtons = document.querySelectorAll('button');
 
-    function displayBoard() {
-        boardButtons.forEach((boardButton, index) => {
-            // Get 2D array index from 1D array
-            let x = Math.floor(index / 3);
-            let y = index % 3;
-            boardButton.textContent = gameBoard.board[x][y];
-        });
-    }
-
+  function displayBoard() {
     boardButtons.forEach((boardButton, index) => {
-        boardButton.addEventListener("click", () => {
-            // Input player move
-            // Update Array
-            // Refresh DOM board from array data
-            // Check for winners
-            console.log("Move");
-        });
+      // Get 2D array index from 1D array
+      const x = Math.floor(index / 3);
+      const y = index % 3;
+      boardButton.textContent = gameBoard.board[x][y];
     });
+  }
 
-    return {
-        displayBoard,
-    };
+  function buttonClicked() {
+    // Input player move
+    // Update Array
+    // Refresh DOM board from array data
+    // Check for winners
+    console.log('Move');
+  }
+
+  function registerButtons() {
+    boardButtons.forEach((boardButton) => {
+      boardButton.addEventListener('click', buttonClicked, {
+        once: true,
+      });
+    });
+  }
+
+  return {
+    displayBoard,
+    registerButtons,
+  };
 })();
 
 displayController.displayBoard();
+displayController.registerButtons();
